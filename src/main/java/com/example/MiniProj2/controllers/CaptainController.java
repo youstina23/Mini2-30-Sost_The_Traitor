919928889def90cb8ce334d328ea84e32dc69f51
youssefbassem1 <youssefbassem12@gmail.com>
2025-04-16 20:12:28 +0200

    Captain

--- src/main/java/com/example/MiniProj2/controllers/CaptainController.java
+++ src/main/java/com/example/MiniProj2/controllers/CaptainController.java
@@ -1,4 +1,45 @@
 package com.example.MiniProj2.controllers;
 
+import com.example.MiniProj2.models.Captain;
+import com.example.MiniProj2.services.CaptainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/captain")
 public class CaptainController {
+
+    private final CaptainService captainService;
+
+    @Autowired
+    public CaptainController(CaptainService captainService) {
+        this.captainService = captainService;
+    }
+
+    @PostMapping("/addCaptain")
+    public Captain addCaptain(@RequestBody Captain captain) {
+        return captainService.addCaptain(captain);
+    }
+
+    @GetMapping("/allCaptains")
+    public List<Captain> getAllCaptains() {
+        return captainService.getAllCaptains();
+    }
+
+    @GetMapping("/{id}")
+    public Captain getCaptainById(@PathVariable Long id) {
+        return captainService.getCaptainById(id);
+    }
+
+    @GetMapping("/filterByRating")
+    public List<Captain> getCaptainsByRating(@RequestParam Double ratingThreshold) {
+        return captainService.getCaptainsByRating(ratingThreshold);
+    }
+
+    @GetMapping("/filterByLicenseNumber")
+    public Captain getCaptainByLicenseNumber(@RequestParam String licenseNumber) {
+        return captainService.getCaptainByLicenseNumber(licenseNumber);
+    }
 }
--- src/main/java/com/example/MiniProj2/repositories/CaptainRepository.java
+++ src/main/java/com/example/MiniProj2/repositories/CaptainRepository.java
@@ -1,4 +1,17 @@
 package com.example.MiniProj2.repositories;
 
-public class CaptainRepository {
+import com.example.MiniProj2.models.Captain;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CaptainRepository extends JpaRepository<Captain, Long> {
+
+    List<Captain> findByAvgRatingScoreGreaterThan(Double threshold);
+
+    Optional<Captain> findByLicenseNumber(String licenseNumber);
 }
+
